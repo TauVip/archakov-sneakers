ec50f5af25dcfe0fc64d7f33722f3c5af2bb8cde
TauVip <Abylai1393@gmail.com>
2022-06-10 18:54:35 +0600

    0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,168 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='wrapper clear'>
+      <div className='overlay'>
+        <div className='drawer'>
+          <h2 className='mb-30'>Корзина</h2>
 
-export default App;
+          <div className='items'>
+            <div className='cartItem d-flex align-center mb-20'>
+              <div
+                style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+                className='cartItemImg'
+              />
+              <div className='mr-20 flex'>
+                <p className='mb-5'>Мужские кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img
+                className='removeBtn'
+                src='/img/btn-remove.svg'
+                alt='Remove'
+              />
+            </div>
+            <div className='cartItem d-flex align-center mb-20'>
+              <div
+                style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+                className='cartItemImg'
+              />
+              <div className='mr-20 flex'>
+                <p className='mb-5'>Мужские кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img
+                className='removeBtn'
+                src='/img/btn-remove.svg'
+                alt='Remove'
+              />
+            </div>
+          </div>
+
+          <ul className='cartTotalBlock'>
+            <li>
+              <span>Итого:</span>
+              <div></div>
+              <b>21 498 руб.</b>
+            </li>
+            <li>
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>1074 руб.</b>
+            </li>
+          </ul>
+          <button>Оформить заказ</button>
+        </div>
+      </div>
+
+      <header className='d-flex justify-between align-center p-40'>
+        <div className='d-flex align-center'>
+          <img width={40} height={40} src='/img/logo.png' alt='Logo' />
+          <div>
+            <h3 className='text-uppercase'>React Sneakers</h3>
+            <p className='opacity-5'>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className='d-flex'>
+          <li className='mr-30'>
+            <img width={18} height={18} src='/img/cart.svg' alt='Cart' />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img width={18} height={18} src='/img/user.svg' alt='Logo' />
+          </li>
+        </ul>
+      </header>
+      <div className='content p-40'>
+        <div className='d-flex align-center justify-between mb-40'>
+          <h1>Все кроссовки</h1>
+          <div className='search-block d-flex'>
+            <img src='/img/search.svg' alt='Search' />
+            <input placeholder='Поиск...' />
+          </div>
+        </div>
+
+        <div className='d-flex'>
+          <div className='card'>
+            <div className='favorite'>
+              <img src='/img/unliked.svg' alt='Unliked' />
+            </div>
+            <img
+              width={133}
+              height={112}
+              src='/img/sneakers/1.jpg'
+              alt='Sneakers'
+            />
+            <h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Цена: </span>
+                <b> 12 999 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src='/img/plus.svg' alt='Plus' />
+              </button>
+            </div>
+          </div>
+
+          <div className='card'>
+            <img
+              width={133}
+              height={112}
+              src='/img/sneakers/2.jpg'
+              alt='Sneakers'
+            />
+            <h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Цена: </span>
+                <b> 12 999 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src='/img/plus.svg' alt='Plus' />
+              </button>
+            </div>
+          </div>
+
+          <div className='card'>
+            <img
+              width={133}
+              height={112}
+              src='/img/sneakers/3.jpg'
+              alt='Sneakers'
+            />
+            <h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Цена: </span>
+                <b> 12 999 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src='/img/plus.svg' alt='Plus' />
+              </button>
+            </div>
+          </div>
+
+          <div className='card'>
+            <img
+              width={133}
+              height={112}
+              src='/img/sneakers/4.jpg'
+              alt='Sneakers'
+            />
+            <h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Цена: </span>
+                <b> 12 999 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src='/img/plus.svg' alt='Plus' />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
+import './index.scss'
+import 'macro-css'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// #2: React Sneakers - создаем простой интернет-магазин (junior) | 1:03:51 / 2:29:14
